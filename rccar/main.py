df89ee52813df560b532d61b778236c2edfe34b8
Satoru Noguchi <strngch@gmail.com>
2019-09-24 22:44:33 +0900

    Refactoring

--- rccar/main.py
+++ rccar/main.py
@@ -2,17 +2,16 @@ import time
 
 import RPi.GPIO as GPIO
 
-
 # MG996R: 50Hz (20ms), 
 #   771(0.771ms) - 2193(2.19ms) 
 #   3.8% - 10.95
-PIN = 23
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(PIN, GPIO.OUT)
-
 NEWTRAL = 6.5
 L_MAX = 4.7
 R_MAX = 9
+
+PIN = 23
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN, GPIO.OUT)
 
 servo = GPIO.PWM(PIN, 50)
 servo.start(NEWTRAL)
@@ -25,7 +24,6 @@ try:
 
         servo.ChangeDutyCycle(L_MAX)
         time.sleep(1)
-
 finally:
     servo.stop()
     GPIO.cleanup()
