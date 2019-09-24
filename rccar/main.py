5d2f8f1bd6cd12cf0b9f73aac016c11a89b09b3a
Satoru Noguchi <strngch@gmail.com>
2019-09-24 22:41:46 +0900

    Add servo example

---
+++ rccar/main.py
@@ -0,0 +1,34 @@
+import time
+
+import RPi.GPIO as GPIO
+
+
+# MG996R: 50Hz (20ms), 
+#   771(0.771ms) - 2193(2.19ms) 
+#   3.8% - 10.95
+PIN = 23
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN, GPIO.OUT)
+
+NEWTRAL = 6.5
+L_MAX = 4.7
+R_MAX = 9
+
+servo = GPIO.PWM(PIN, 50)
+servo.start(NEWTRAL)
+time.sleep(1)
+
+try:
+    while True:
+        servo.ChangeDutyCycle(R_MAX)
+        time.sleep(1)
+
+        servo.ChangeDutyCycle(L_MAX)
+        time.sleep(1)
+
+finally:
+    servo.stop()
+    GPIO.cleanup()
+
+print('done')
+
